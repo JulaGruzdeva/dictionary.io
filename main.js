33788d5d3872e03c3cfd656f4322dca33e9d7e29
Julia Gruzdeva <yulasha.gruzdeva@gmail.com>
2022-01-23 21:34:58 +0300

    change js&&html&&css

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const butNo = document.querySelector('#No');
 const butAdd = document.querySelector('#Add');
 const inputTranslate = document.querySelector('#inputTranslate');
 const inputWord = document.querySelector('#inputWord'); 
+const workList = document.querySelector('.worklist');
 
 buttonAdd.addEventListener('click',()=>{
     buttonAdd.style.display = 'none';
@@ -13,25 +14,33 @@ buttonAdd.addEventListener('click',()=>{
 butNo.addEventListener('click',()=>{
     buttonAdd.style.display = 'block';
     addBanner.style.display = 'none';
+    inputWord.value = '';
+    inputTranslate.value = '';
 })
 
 butAdd.addEventListener('click',()=>{
-    localStorage.setItem(inputWord.value,inputTranslate.value);
+    if(inputWord.value === '' || inputTranslate.value === '' || !inputTranslate.value.trim() || !inputWord.value.trim()){
+        return false
+    } else{
+        localStorage.setItem(inputWord.value.trim(),inputTranslate.value.trim());
+    }
     inputWord.value = '';
     inputTranslate.value = '';
     buttonAdd.style.display = 'block';
     addBanner.style.display = 'none';
+    buildWorkList();
+
 })
 
 document.addEventListener('click',(item)=>{
     if(item.target.classList.contains('buttonShow') && item.target.id != 'Add' && item.target.id != 'No'){
 
         if (item.target.innerHTML == 'Show'){
-            const translete = document.createElement('p');
-            translete.innerHTML = 'MOZG';
-            translete.classList.add('blockWithoutTranslateText');
             let itemParent = item.target.parentNode.parentNode.firstElementChild;
-            itemParent.append(translete);
+            const translate = document.createElement('p');
+            translate.innerHTML = `${localStorage[itemParent.firstElementChild.innerHTML]}`;
+            translate.classList.add('blockWithoutTranslateText');
+            itemParent.append(translate);
             item.target.innerHTML = 'Hide';
         }
         
@@ -45,6 +54,15 @@ document.addEventListener('click',(item)=>{
 
 })
 
+document.addEventListener('click',(item)=>{
+    let delItemParent = item.target.parentNode.parentNode.firstElementChild.firstElementChild;
+    
+    if(item.target.classList.contains('buttonDel')){
+        item.target.parentNode.parentNode.parentNode.removeChild(item.target.parentNode.parentNode);
+        localStorage.removeItem(`${delItemParent.innerHTML}`)
+    }
+})
+
 function atrToNum(x){
     let res = '';
     for(i of x){
@@ -53,4 +71,23 @@ function atrToNum(x){
         }
     }
     return Number(res);
+}
+
+buildWorkList();
+
+function buildWorkList(){
+    workList.innerHTML = '';
+    for(let i=0; i<localStorage.length; i++){
+        const newBlock = document.createElement('div');
+        newBlock.classList.add('blockWithoutTranslate');
+        newBlock.innerHTML = `<div style="display: inline-block;">
+        <p class="blockWithoutTranslateText">${localStorage.key(i)}</p>
+    </div>
+    <div style="display: inline-block;">
+        <button class="buttonDel" style="margin-left: 168px;">X</button>
+        <button class="buttonShow" style="margin-left: 4px;">Show</button>
+    </div>`;
+        workList.append(newBlock);
+        // console.log(localStorage.key(i),localStorage[localStorage.key(i)]);
+    }
 }
